3c2cd647676e3f87ac310b11b2c2d5d1b9fc307b
can7wysiwyg <paulkssa@gmail.com>
2023-09-11 13:53:58 +0200

    updated code

--- src/components/pages/MyInfo.js
+++ src/components/pages/MyInfo.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react"
 import { GlobalState } from "../GlobalState"
+import "./myinfo.css"
 import axios from "axios"
+import moment from "moment/moment"
 
 function MyInfo() {
    const state = useContext(GlobalState)
@@ -17,7 +19,7 @@ const res = await axios.get(`/userroute/user`, {
 
 })
 
-console.log(res.data);
+setPerson(res.data);
 
     }
 
@@ -25,7 +27,28 @@ console.log(res.data);
 
    }, [usertoken])
 
+
+   console.log(person);
+
     return(<>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <div className="library-card text-center">
+            <h2>Member Card</h2>
+            <img src={person.userImage} alt={person.fullname} className="img-fluid mt-3 mb-3" />
+            <p>Name: {person.fullname}</p>
+            <p>Birthday: {person.DOB} </p>
+            <p>Member ID: {person.idNumber} </p>
+            <p>Joined: { moment(person.createdAt).format("MMM D YYYY") }</p>
+            <p>Email: {person.email} </p>
+            <p>Home: {person.location} </p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     
     
     </>)
